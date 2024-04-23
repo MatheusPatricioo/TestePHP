91a161ab5e3414023a93846c1506f6f74c31c0ca
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-23 09:35:29 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,5 +1,7 @@
 /*
-Como chegar na pasta que o servidor acessa diretamente no meu pc
+Essa pasta é a pasta oficial do servidor pro meu computador, ou  seja
+tudo oq terceiros quiserem acessar do meu servidor, vai ser oq tiver dentro
+desta pasta : localhost
 */
 
 // 127.0.0.1
