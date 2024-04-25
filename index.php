96d471f88eeac8763b079b4931aa56a21fe503f1
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-25 13:33:23 -0300

    up

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$ingredientes = [
+    'açucar',
+    'farinho de trigo',
+    'ovo',
+    'leite',
+    'fermento em pó',
+];
+
+// Usando implode com "\n" como delimitador para quebrar cada ingrediente em uma linha separada
+echo implode("\n", $ingredientes);
+
+foreach()
