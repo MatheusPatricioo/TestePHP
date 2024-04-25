0d2fee129197a2e3ca78df0d3da7737a374b36e4
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-25 11:28:48 -0300

    up

---
+++ switch.php
@@ -0,0 +1,14 @@
+<?php
+$tipo = 'foto';
+
+switch($tipo) {
+    case 'foto':
+        echo'Exibindo FOTO';
+        break;
+    case 'video':
+        echo'Exibindo VIDEO';
+        break;
+    case 'texto':
+        echo'Exibindo TEXTO';
+        break;
+};
