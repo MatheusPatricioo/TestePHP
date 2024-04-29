a80692cdf8ff067abd54d84f5cf0cf4c0d22b5ff
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-29 09:37:16 -0300

    up

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 function subsequente(){
-    for($q=0; $q<10;$q++){
+    for($q=0; $q<12;$q++){
         echo $q."<br/>";
     }
     echo "<hr/>";
 }
 
-subsequente();
+function main(){
+    echo "Chamando subsequente() de dentro da main():<br/>";
+    subsequente();
+}
+
+main(); // Chama a função main()
