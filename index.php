aa8858917e6467da0ea55ad25232b7f8a5c23634
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-04-29 13:54:34 -0300

    up

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+/*
 function recursividade($a, $b){
     $recurs = $a / $b;
 
@@ -9,3 +10,15 @@ function recursividade($a, $b){
 }
 
 recursividade(500, 2);
+*/
+
+function recursividade($a, $b, $contador){
+    $contador--;
+    $recurs = $a / $b;
+    echo (round($recurs))."<br/>";
+    if($contador > 0){
+        recursividade($recurs, $b, $contador);
+    }
+}
+
+recursividade(400, 2, 20);
